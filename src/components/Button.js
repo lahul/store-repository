4d07b9d1a0b8f0f8d2e2440883230fc2f52463f8
lahul <lahuldcosta04@gmail.com>
2019-01-26 13:26:22 +0530

    handle and add to cart function

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,6 +5,8 @@ text-transform:capitalize;
 font-size : 1.4rem;
 background:transparent;
 border : 0.5rem solid var(--lightBlue);
+border-color : ${props => props.cart?"var(--mainYellow)":"var(--lightBlue)"};
+color:${prop => prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
 color : var(--lightBlue);
 border-radius:0.5rem;
 padding:0.2rem 0.5rem;
@@ -12,6 +14,8 @@ cursor:pointer;
 margin:0.2rem 0.5rem 0.2rem 0;
 transition : all 0.5s ease-in-out;
 &:hover{
+    background:${prop => prop.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+color : var(--lightBlue);
     background:var(--lightBlue);
     color:car(--mainBlue);
 }

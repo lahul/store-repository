484b9ff58531fcc04c6b6cba3f7a7f2b46436b75
lahul <lahuldcosta04@gmail.com>
2019-01-27 16:17:23 +0530

    Modal design completed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,7 +10,7 @@ export default class Modal extends Component {
             <ProductConsumer>
                 {(value) => {
                     const { modalOpen, closeModal } = value;
-                    const {ing, title,price} =value.modalProduct;
+                    const {img, title,price} =value.modalProduct;
                     if(!modalOpen){
                         return null;
                     }
@@ -18,8 +18,21 @@ export default class Modal extends Component {
                         return (<ModalContainer>
                             <div className="container">
                                 <div className="row">
-                                    <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
+                                    <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
                                         <h5>item added to the cart</h5>
+                                        <img src={img} className="img-fluid" alt="product"/>
+                                        <h5>{title}</h5>
+                                        <h5 className="text-muted">Price : $ {price}</h5>
+                                        <Link to='/'>
+                                        <ButtonContainer onClick={() =>{closeModal()}}> 
+                                            continue shopping
+                                        </ButtonContainer>
+                                        </Link>
+                                        <Link to='/cart'>
+                                        <ButtonContainer cart onClick={() =>{closeModal()}}> 
+                                            go to cart
+                                        </ButtonContainer>
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
@@ -33,5 +46,17 @@ export default class Modal extends Component {
 }
 
 const ModalContainer = styled.div`
+position : absolute;
+top : 0;
+left : 0;
+bottom : 0;
+right : 0;
+background : rgba(0,0,0,0.3);
+display : flex;
+align-items : center;
+justify-content : center;
+#modal{
+    background : var(--mainWhite);
 
+}
 `;
